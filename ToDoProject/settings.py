f0c627949d5380fc4839644424e794450e70d10c
Kevin Malomba <malombakevin@gmail.com>
2022-09-11 17:47:13 +0300

    fix bug :applicatin error

--- ToDoProject/settings.py
+++ ToDoProject/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3_2755ok3d0f5*h2()@sbsj=*b#olvw9--kwk#v5z3@)mty(5h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['to-do-app-bykev.herokuapp.com']
 
 
 # Application definition
